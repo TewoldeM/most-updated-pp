49cdc5a7a0c336f89bc93e071e061a8419804710
tewolde marie <tewoldemarie6@gmail.com>
2025-11-16 23:04:20 +0300

    fix the postcss error

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "imljzgcuelzzzncfzlnc.supabase.co",
-      },
-    ],
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
